8ae5046107ba952d8b6a033033e2fac53a66124a
connor <connormullett@gmail.com>
2019-06-05 16:14:28 -0400

    learning CORE

---
+++ Notes.Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Notes.Services/NoteService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Notes.Models;
+using System.Collections.Generic;
+
+namespace Notes.Services
+{
+    public class NoteService
+    {   
+        private List<Note> _notes = new List<Note>();
+
+        public List<Note> GetNotes()
+        {
+            return _notes;
+        }
+    }
+}
